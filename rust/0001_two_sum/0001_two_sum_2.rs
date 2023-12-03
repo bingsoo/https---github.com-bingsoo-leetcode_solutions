6b7a13d05c58546f1a45c5b569012e97e974d4f9
bingsoo <bingsoo@bingsoo.com>
2023-12-02 20:06:27 -0800

    initial commit with two sum solutions

---
+++ rust/0001_two_sum/0001_two_sum_2.rs
@@ -0,0 +1,16 @@
+use std::collections::HashMap;
+
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut m = HashMap::new();
+        for (i, &num) in nums.iter().enumerate() {
+            match m.get(&(target - num)) {
+                Some(&j) => return vec![i as i32, j as i32],
+                None => {
+                    m.insert(num, i as i32);
+                }
+            }
+        }
+        unreachable!()
+    }
+}
